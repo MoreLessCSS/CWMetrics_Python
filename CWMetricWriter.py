a0b9c06dcd03c1cf1df29a85d45f35b2427d3b9f
Robert Franjkovic <Robert.Franjkovic@deutschebahn.com>
2017-08-06 18:25:00 +0200

    initial

--- CWMetricWriter.py
+++ CWMetricWriter.py
@@ -1,28 +1,22 @@
 from boto.ec2 import cloudwatch
 from boto.utils import get_instance_metadata
-from datetime import *
-import pprint
-
 
 class CWMetricWriter(object):
 
     def __init__(self, region):
         self.connection = cloudwatch.connect_to_region(
-            'eu-central-1',
-            aws_access_key_id='AKIAICPDUK5NKKB3XLIQ',
-            aws_secret_access_key='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'
+            'eu-central-1'
             )
-
-
-    def send_metrics(self, varNamespace, instanceId, instanceType, varMetric, varValue, unit, dimensions):
-        self.connection.put_metric_data(varNamespace, varMetric,
-                                    varValue, unit=unit,
-                                    dimensions=dimensions)
-
 
     def _get_instance_metadata(self):
         metadata = get_instance_metadata()
-        #for keys,values in metadata.items():
-        #    print(keys)
-        #    print(values)
         return metadata['instance-id'], metadata['instance-type'], metadata['hostname']
+
+    def send_metrics(self, varNamespace, instanceId, instanceType, varMetric, varValue, unit, dimensions):
+        self.connection.put_metric_data(
+                                        varNamespace,
+                                        varMetric,
+                                        varValue,
+                                        unit=unit,
+                                        dimensions=dimensions
+                                        )
--- metrics.py
+++ metrics.py
@@ -1,11 +1,5 @@
-from datetime import *
 import loadConfig
-import time
 from CWMetricWriter import *
-from inspect import getmembers
-from pprint import pprint
-from boto.ec2 import cloudwatch
-from boto.utils import get_instance_metadata
 
 config = loadConfig.getConfigFile()
 client = CWMetricWriter(config['region'])
@@ -21,16 +15,9 @@ for metric in config['metrics']:
         mod = __import__(moduleName, fromlist=[moduleName])
         module = getattr(mod, moduleName)
         var = module(metric[moduleConfig], moduleName)
-
         metricValue = var.getMetric()
         units = var.getUnit()
-        #print ("\n")
-        #pprint (config['metrics'])
-
         client = CWMetricWriter(config['region'])
         value = client.send_metrics('varNamespace', 'instanceId', 'instanceType', moduleConfig, metricValue, units, dimensions)
-
-
-
 
         #metric[moduleConfig]['namespace'], MetricData
