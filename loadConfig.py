3aef46ba02891b785609c82849e65761237c635a
Robert Franjkovic <Robert.Franjkovic@deutschebahn.com>
2017-08-06 21:16:08 +0200

    initial

--- loadConfig.py
+++ loadConfig.py
@@ -8,6 +8,7 @@ def getConfigFile():
          appDir = os.path.dirname(os.path.abspath(__file__))
          f=str(appDir)
          filename = (f + '/config/MasterNode/config.json')
+         filename=str(filename)
          print ("filename:" + filename + "\n")
          with open(filename) as data_file:
                 data = json.load(data_file)
