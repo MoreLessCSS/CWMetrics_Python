251668aeb2651fc6bf98e0e5ba66ab96c5de9b68
Robert Franjkovic <Robert.Franjkovic@deutschebahn.com>
2017-08-06 20:32:07 +0200

    initial

--- CWMetricWriter.py
+++ CWMetricWriter.py
@@ -4,15 +4,31 @@ from boto.utils import get_instance_metadata
 class CWMetricWriter(object):
 
     def __init__(self, region):
-        self.connection = cloudwatch.connect_to_region(region)
+        try:
+            self.connection = cloudwatch.connect_to_region(region)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+        return result
 
     def _get_instance_metadata(self):
-        metadata = get_instance_metadata()
-        return metadata['instance-id'], metadata['instance-type'], metadata['hostname']
+        try:
+            metadata = get_instance_metadata()
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+        else:
+            return metadata['instance-id'], metadata['instance-type'], metadata['hostname']
 
     def send_metrics(self, varNamespace, instanceId, instanceType, varMetric, varValue, unit, dimensions):
-        self.connection.put_metric_data(varNamespace,
-                                        varMetric,
-                                        varValue,
-                                        unit=unit,
-                                        dimensions=dimensions)
+        try:
+            self.connection.put_metric_data(varNamespace,
+                                                    varMetric,
+                                                    varValue,
+                                                    unit=unit,
+                                                    dimensions=dimensions)
+        except:
+            print "Unexpected error:", sys.exc_info()[0]
+            raise
+        return result
+
