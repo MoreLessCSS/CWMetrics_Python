4fe927c6b5f70920b2c85706a408e2c95cb62d3f
Robert Franjkovic <Robert.Franjkovic@deutschebahn.com>
2017-08-06 20:59:34 +0200

    initial

--- loadConfig.py
+++ loadConfig.py
@@ -5,9 +5,9 @@ import os
 
 def getConfigFile():
     try:
-        appDir = os.path.dirname(os.path.abspath(__file__))
-        filename = os.path.join(appDir, '/config/MasterNode/config.json')
-        with open(filename) as data_file:
+         appDir = os.path.dirname(os.path.abspath(__file__))
+         filename = os.path.join(appDir, '/config/MasterNode/config.json')
+         with open(filename) as data_file:
                 data = json.load(data_file)
     except IOError as (errno, strerror):
          print "I/O error({0}): {1}".format(errno, strerror)
@@ -15,4 +15,4 @@ def getConfigFile():
          print "Unexpected error:", sys.exc_info()[0]
          raise
     else:
-        return data
+         return data
