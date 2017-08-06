26e7da58b8905bce3e1892a109426fd0046410a6
Robert Franjkovic <Robert.Franjkovic@deutschebahn.com>
2017-08-06 19:15:16 +0200

    initial

--- loadConfig.py
+++ loadConfig.py
@@ -1,7 +1,14 @@
 import json
+import sys
 
 def getConfigFile():
+ try:
     with open("config.json") as data_file:
         data = json.load(data_file)
         return data
 
+ except IOError as (errno, strerror):
+     print "I/O error({0}): {1}".format(errno, strerror)
+ except:
+     print "Unexpected error:", sys.exc_info()[0]
+     raise
